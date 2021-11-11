867c654d567e822cb04f3809a8fe9c7ee64f03e8
LingDad <xujianwei919@gmail.com>
2021-11-11 21:02:56 +0800

    add solution for pro1-18

---
+++ pro1-18.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define MAX 1000
+
+int getOneLine(char s[], int max);
+
+int main()
+{
+    int i, len;
+    char line[MAX];
+
+    while ((len = getOneLine(line, MAX)) > 0)
+    {
+        for (i = len - 1; i > 0 && (line[i] == ' ' || line[i] == '\t'); i--)
+        {
+            line[i] = 'A';
+            line[i+1] = '\0';
+        }
+        printf("%s", line);
+    }
+    return 0;
+}
+
+int getOneLine(char s[], int max)
+{
+    int i, c;
+
+    for (i = 0; i < max - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+        s[i] = c;
+    if (c == '\n')
+    {
+        s[i] = '\n';
+        i++;
+    }
+    s[i] = '\0';
+    return i;
+}
